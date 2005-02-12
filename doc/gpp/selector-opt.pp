d1f432162f3ebc4557f44d5b6a630131bba8684f
mbravenboer <mbravenboer>
2005-02-12 18:14:17 +0000

    Added Box and PP-table documentation

---
+++ doc/gpp/selector-opt.pp
@@ -0,0 +1,2 @@
+Return       -- H [ KW["return"] _1 ";" ]
+Return.1:opt -- H [ _1 ]
---
+++ doc/gpp/selectors.pp
@@ -0,0 +1,18 @@
+[
+  ex1                  -- _1,
+  ex1.1:opt            -- _1,
+  ex2                  -- _1,
+  ex2.1:iter           -- _1,
+  ex3                  -- _1,
+  ex3.1:iter-star      -- _1,
+  ex4                  -- _1,
+  ex4.1:iter-sep       -- _1 _2,
+  ex5                  -- _1,
+  ex5.1:iter-star-sep  -- _1 _2,
+  ex6                  -- _1,
+  ex6.1:alt            -- KW["one"] KW["two"] _1,
+  ex6.1:alt.1:opt      -- _1,
+  ex7                  -- _1,
+  ex7.1:seq            -- KW["one"] KW["two"] _1,
+  ex7.1:seq.1:opt      -- _1
+]
